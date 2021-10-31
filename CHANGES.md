bfe60b8cbfa4facc60af8bf36ec031b3cb7d7ae5
T. Kim Nguyen <kim@sixfeetup.com>
2021-10-31 11:16:56 -0500

    add initial AWS Terraform plan, update Linode StackScript with contents of provision_linode.sh, remove provision_linode.sh

--- CHANGES.md
+++ CHANGES.md
@@ -1,3 +1,10 @@
+Unreleased
+----------
+
+- add initial AWS Terraform plan, update Linode StackScript with contents of provision_linode.sh, remove provision_linode.sh
+  [tkimnguyen]
+
+
 1.0 (2020-12-10)
 ----------------
 
---
+++ src/aws.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.13.0"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-09e67e426f25ce0d7"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Plone in a Box™"
+  }
+}
+
